880ebea4f39cf3752f56bd45b3caabfc6678b6d8
John Mastro <john.b.mastro@gmail.com>
2018-06-15 16:14:31 -0700

    Add cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const path = require('path');
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 
 require('dotenv').config();
 
 const PORT = process.env.PORT || 9000;
+
+const corsOptions = {
+  origin: 'http://75.101.193.74:5500',
+  optionsSuccessStatus: 200
+};
 
 const app = express();
 
